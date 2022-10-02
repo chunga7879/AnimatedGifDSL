ed8244abd26dbd8cd8edfb97464b2688cca1e253
Shiven Shukla <shiven@students.cs.ubc.ca>
2022-10-01 17:02:53 -0700

    adds save to built-in functions

--- src/core/values/Array.java
+++ src/core/values/Array.java
@@ -5,14 +5,23 @@ import java.util.ArrayList;
 public class Array extends Value{
     public static final String NAME = "Array";
 
-    private final ArrayList<Value> a;
+    private final ArrayList<Value> arr;
 
     public Array() {
         super(Array.NAME);
-        this.a = new ArrayList<>();
+        this.arr = new ArrayList<>();
     }
 
     public void add(Value v) {
-       this.a.add(v);
+       this.arr.add(v);
+    }
+
+    public ArrayList<Value> get() {
+        return this.arr;
+    }
+
+    @Override
+    public Array asArray() {
+        return this;
     }
 }
