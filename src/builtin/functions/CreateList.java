ab6c3f0ff62d745d731187938f6112e3c84ce77b
Shiven Shukla <shiven@students.cs.ubc.ca>
2022-10-08 13:44:41 -0700

    changes return type to array

--- src/builtin/functions/CreateList.java
+++ src/builtin/functions/CreateList.java
@@ -3,17 +3,14 @@ package builtin.functions;
 import core.Scope;
 import core.values.AbstractFunction;
 import core.values.Array;
-import core.values.Null;
 import core.values.Value;
 
 /**
- * Creates an empty list and assigns it to a variable
+ * Creates an empty list
  */
 public class CreateList extends AbstractFunction {
     @Override
     public Value call(Scope scope) {
-        // TODO: fix name parameter
-        scope.setVar("array", new Array());
-        return Null.NULL;
+        return new Array();
     }
 }
