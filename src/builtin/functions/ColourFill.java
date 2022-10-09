3d63c5df985d7b48eb6454aaaef5d4ba87f2e58d
katherine.liang <katherine.liang.0@gmail.com>
2022-10-09 15:40:33 -0700

    Added ColourFill class.

---
+++ src/builtin/functions/ColourFill.java
@@ -0,0 +1,14 @@
+package builtin.functions;
+
+import core.Scope;
+import core.values.AbstractFunction;
+import core.values.Colour;
+import core.values.Null;
+import core.values.Value;
+
+public class ColourFill extends AbstractFunction {
+    @Override
+    public Value call(Scope scope) {
+        return Null.NULL;
+    }
+}
