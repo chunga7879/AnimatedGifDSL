6df3d6be1cfa7bc2a41b99e6cbd997e0221fcdea
katherine.liang <katherine.liang.0@gmail.com>
2022-10-10 12:17:19 -0700

    Changed variable name back, updated tests.

--- src/test/filtering/FilterTest.java
+++ src/test/filtering/FilterTest.java
@@ -24,103 +24,102 @@ public class FilterTest {
             scope = new Scope();
             scope.setVar("$target", seal);
         } catch (IOException ioException) {
-            Assertions.assertTrue(false);
+            Assertions.fail(ioException.getMessage());
         }
     }
 
     @Test
     public void filterSepia() {
         try {
-            scope.setVar("filtering", new StringValue("sepia"));
+            scope.setVar("filter", new StringValue("sepia"));
 
             Filter filter = new Filter();
             Image italy = (Image) filter.call(scope);
 
             italy.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/sepiaItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterChrome() {
         try {
-            scope.setVar("filtering", new StringValue("chrome"));
+            scope.setVar("filter", new StringValue("chrome"));
 
             Filter filter = new Filter();
             Image seal = (Image) filter.call(scope);
 
             seal.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/chromeItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterBlur() {
         try {
-            scope.setVar("filtering", new StringValue("blur"));
+            scope.setVar("filter", new StringValue("blur"));
 
             Filter filter = new Filter();
             Image seal = (Image) filter.call(scope);
 
             seal.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/blurItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterGreyscale() {
         try {
-            scope.setVar("filtering", new StringValue("greyscale"));
+            scope.setVar("filter", new StringValue("greyscale"));
 
             Filter filter = new Filter();
             Image seal = (Image) filter.call(scope);
 
             seal.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/greyScaleItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterSharpen() {
         try {
-            scope.setVar("filtering", new StringValue("sharpen"));
+            scope.setVar("filter", new StringValue("sharpen"));
 
             Filter filter = new Filter();
             Image seal = (Image) filter.call(scope);
 
             seal.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/sharpenItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterInvert() {
         try {
-            scope.setVar("filtering", new StringValue("invert"));
+            scope.setVar("filter", new StringValue("invert"));
 
             Filter filter = new Filter();
             Image seal = (Image) filter.call(scope);
 
             seal.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/invertItaly.png"));
         } catch (IOException exception) {
-            Assertions.assertTrue(false);
+            Assertions.fail(exception.getMessage());
         }
     }
 
     @Test
     public void filterInvalid() {
         try {
-            scope.setVar("filtering", new StringValue("summer"));
+            scope.setVar("filter", new StringValue("summer"));
 
             Filter filter = new Filter();
-            Image seal = (Image) filter.call(scope);
-
-            Assertions.assertTrue(false);
+            filter.call(scope);
+            Assertions.fail();
         } catch (IllegalArgumentException exception) {
             Assertions.assertTrue(true);
         }
