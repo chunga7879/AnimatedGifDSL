56739de38bf1ef9fee46e0969d335e126447ce58
katherine.liang <katherine.liang.0@gmail.com>
2022-10-09 20:55:09 -0700

    Added SetOpacity and tests.

---
+++ src/builtin/functions/SetOpacity.java
@@ -0,0 +1,27 @@
+package builtin.functions;
+
+import com.sksamuel.scrimage.ImmutableImage;
+import com.sksamuel.scrimage.color.RGBColor;
+import core.Scope;
+import core.values.*;
+
+public class SetOpacity extends AbstractFunction {
+    @Override
+    public Value call(Scope scope) {
+        ImmutableImage image = scope.getVar("$target").asImage().get();
+        IntegerValue opacityAmount = scope.getVar("amount").asInteger();
+
+        if (opacityAmount.get() > 100 || opacityAmount.get() < 0) {
+            throw new IllegalArgumentException("Opacity value should be within [0, 100]");
+        }
+
+        // scrimage library accepts alpha range from 0 - 255
+        int scaledOpacity = (int) (opacityAmount.get() * 2.55);
+
+        ImmutableImage transparentImage = image.map((p) -> {
+            return new RGBColor(p.red(), p.green(), p.blue(), scaledOpacity).awt();
+        });
+
+        return new Image(transparentImage);
+    }
+}
---
+++ src/test/filtering/SetOpacityTest.java
@@ -0,0 +1,72 @@
+package filtering;
+
+import builtin.functions.SetOpacity;
+import com.sksamuel.scrimage.ImmutableImage;
+import com.sksamuel.scrimage.nio.PngWriter;
+import core.Scope;
+import core.values.Image;
+import core.values.IntegerValue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SetOpacityTest {
+
+    @Test
+    public void setOpacityTest80() {
+        try {
+            ImmutableImage catMouth = ImmutableImage.loader().fromFile("src/test/filtering/testInputs/cat.png");
+            Image catImage = new Image(catMouth);
+            Scope scope = new Scope();
+            scope.setVar("$target", catImage);
+            scope.setVar("amount", new IntegerValue(80));
+
+            SetOpacity opacityFunction = new SetOpacity();
+            Image transparentCat = (Image) opacityFunction.call(scope);
+
+            transparentCat.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/transparentCat80.png"));
+        } catch (IOException exception) {
+            Assertions.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void setOpacityTest0() {
+        try {
+            ImmutableImage catMouth = ImmutableImage.loader().fromFile("src/test/filtering/testInputs/cat.png");
+            Image catImage = new Image(catMouth);
+            Scope scope = new Scope();
+            scope.setVar("$target", catImage);
+            scope.setVar("amount", new IntegerValue(0));
+
+            SetOpacity opacityFunction = new SetOpacity();
+            Image transparentCat = (Image) opacityFunction.call(scope);
+
+            transparentCat.get().output(PngWriter.NoCompression, new File("src/test/filtering/testResults/transparentCat.png"));
+        } catch (IOException exception) {
+            Assertions.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void setOpacityInvalidValue() {
+        try {
+            ImmutableImage catMouth = ImmutableImage.loader().fromFile("src/test/filtering/images/cat.png");
+            Image catImage = new Image(catMouth);
+            Scope scope = new Scope();
+            scope.setVar("$target", catImage);
+            scope.setVar("amount", new IntegerValue(110));
+
+            SetOpacity opacityFunction = new SetOpacity();
+            Image transparentCat = (Image) opacityFunction.call(scope);
+
+            Assertions.assertTrue(false);
+        } catch (IOException exception) {
+            Assertions.assertTrue(false);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Assertions.assertTrue(true);
+        }
+    }
+}
