32a78742ef6f52a2fdc37f02f7550234bdf81922
chunga <chunga7879@gmail.com>
2022-10-09 20:53:02 -0700

    h, w test complete

---
+++ src/test/image/GetWidthTest.java
@@ -0,0 +1,30 @@
+package image;
+
+import builtin.functions.GetHeight;
+import builtin.functions.GetWidth;
+import com.sksamuel.scrimage.ImmutableImage;
+import core.Scope;
+import core.values.Image;
+import core.values.IntegerValue;
+import files.filesystem.FileSystem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetWidthTest {
+
+    @Test
+    public void getHeightSuccess() throws FileNotFoundException {
+        Scope scope = new Scope();
+        ImmutableImage dvd = FileSystem.openImage("dvd-logo.png");
+        scope.setVar("$target", new Image(dvd));
+        GetWidth getWidth = new GetWidth();
+        IntegerValue width = (IntegerValue) getWidth.call(scope);
+
+        Assertions.assertEquals(dvd.width, width.get());
+    }
+
+}
